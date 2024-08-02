aabd7e0a0846050d6852322e2354a6d4072bffb6
pahntm000p <tgeopen@gmail.com>
2024-08-02 13:53:42 +0000

    Nice

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 # config.py
 api_id = "20028561"
 api_hash = "0f3793daaf4d3905e55b0e44d8719cad"
-bot_token = "6617412135:AAFc7F-H7xoNbDMHTO8y-MrKWiurymgGwT8"
+bot_token = "7057616627:AAHfj_fMLLraUvND121ARAYUKrkVVad8X0U"
 MONGO_URL = "mongodb+srv://tmaid6012:loniko0908@cluster0.e3nhbm0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 OWNER_ID = 5630057244
 DEV_USERS = [5630057244]
